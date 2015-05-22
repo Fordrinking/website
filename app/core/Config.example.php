f68101ab9d1f781664fb5b782ca0cc6ff47a6b92
Hill1942 <ykdacd123@gmail.com>
2015-05-22 16:02:39 +0800

    format change

--- app/core/Config.example.php
+++ app/core/Config.example.php
@@ -13,51 +13,51 @@ use helpers\Session;
 
 class Config {
 
-	public function __construct() {
+    public function __construct() {
 
-		//turn on output buffering
-		ob_start();
+        //turn on output buffering
+        ob_start();
 
-		//site address
-		define('DIR', 'http://example.com/');
+        //site address
+        define('DIR', 'http://example.com/');
 
-		//set default controller and method for legacy calls
-		define('DEFAULT_CONTROLLER', 'home');
-		define('DEFAULT_METHOD' , 'index');
+        //set default controller and method for legacy calls
+        define('DEFAULT_CONTROLLER', 'home');
+        define('DEFAULT_METHOD' , 'index');
 
-		//set a default language
-		define('LANGUAGE_CODE', 'en');
+        //set a default language
+        define('LANGUAGE_CODE', 'en');
 
-		//database details ONLY NEEDED IF USING A DATABASE
-		define('DB_TYPE', 'mysql');
-		define('DB_HOST', 'localhost');
-		define('DB_NAME', 'db_name');
-		define('DB_USER', 'user');
-		define('DB_PASS', 'password');
-		define('PREFIX', 'prefix_');
+        //database details ONLY NEEDED IF USING A DATABASE
+        define('DB_TYPE', 'mysql');
+        define('DB_HOST', 'localhost');
+        define('DB_NAME', 'db_name');
+        define('DB_USER', 'user');
+        define('DB_PASS', 'password');
+        define('PREFIX', 'prefix_');
 
-		//set prefix for sessions
-		define('SESSION_PREFIX', 'prefix_');
+        //set prefix for sessions
+        define('SESSION_PREFIX', 'prefix_');
 
-		//optionall create a constant for the name of the site
-		define('SITETITLE', 'XXX');
+        //optionall create a constant for the name of the site
+        define('SITETITLE', 'XXX');
 
-		define('LOCAL_DIR', 'your local path');
+        define('LOCAL_DIR', 'your local path');
 
-		//turn on custom error handling
-		set_exception_handler('core\logger::exception_handler');
-		set_error_handler('core\logger::error_handler');
+        //turn on custom error handling
+        set_exception_handler('core\logger::exception_handler');
+        set_error_handler('core\logger::error_handler');
 
-		//set timezone
-		date_default_timezone_set('Asia/Shanghai');
+        //set timezone
+        date_default_timezone_set('Asia/Shanghai');
 
-		//start sessions
-		Session::init();
+        //start sessions
+        Session::init();
 
-		//set the default template
-		Session::set('template', 'default');
+        //set the default template
+        Session::set('template', 'default');
 
-		Dao::openDB();
-	}
+        Dao::openDB();
+    }
 
 }
