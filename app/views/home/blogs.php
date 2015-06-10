e81107764c484e6c89ccc5b59be716df56d07ea8
Hill1942 <ykdacd123@gmail.com>
2015-06-10 15:37:47 +0800

    comment module

--- app/views/home/blogs.php
+++ app/views/home/blogs.php
@@ -27,6 +27,30 @@
                         <div class='blog-f-btn blog-comment-btn'><i class='fa fa-comment-o fa-lg'></i></div>
                         <div class='blog-f-btn blog-like-btn'><i class='fa fa-thumbs-o-up fa-lg'></i></div>
                     </div>
+                    <div class="blog-extra">
+                        <div class="blog-comment-header">
+                            <div class="blog-comment-user">
+                                <img class='user-img left' src='<?php echo $row->getAvatar(); ?>'/>
+                            </div>
+                            <div class="blog-comment-input">
+                                <div><input type="text"></div>
+                                <div>
+                                    <i class="fa fa-smile-o fa-lg"></i>
+                                    <i class="fa fa-file-image-o fa-lg"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-comment-body">
+                            <div class="blog-comment-item">
+                                <div><a href="#">user</a></div>
+                                <div></div>
+                                <div>
+                                    <div class="blog-comment-date"></div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         <?php
